3c94e05c0c0fb72d3479b5d30a171d009b9b772e
Andreea <Andreea>
2017-01-08 23:15:10 +0200

    Final Update

--- test_proiect.ino
+++ test_proiect.ino
@@ -23,7 +23,6 @@ int in4 = 7;
 
 int speedA = 0;
 int speedB = 0;
-int directionMovement = 0;
 bool isStateChange = true;
 
 //telecomanda
@@ -41,133 +40,107 @@ void setup() {
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
   irrecv.enableIRIn(); // Start the receiver
-  Serial.begin(9600);
 }
 
 void loop() {
  if (irrecv.decode(&auxResults)) {
-      //Serial.println(auxResults.value,HEX);
       isStateChange = true;
       irrecv.resume();
  }
  else{
-    isStateChange = false;
+     isStateChange = false;
  }
 
  if (auxResults.value == front || auxResults.value == back || auxResults.value == left || auxResults.value == right || auxResults.value == powerOffOn || 
      auxResults.value == repeat || auxResults.value == plus || auxResults.value == minus  || auxResults.value == rotateLeft || auxResults.value == rotateRight){
-     results = auxResults;
+         results = auxResults;
  }
 
  if (results.value == front){ 
  //ambele motoare merg inainte
- speedA = 110;
- speedB = 110;
- directionMovement = 0;
- digitalWrite(in3, HIGH);
- digitalWrite(in4, LOW);
- analogWrite(enB, speedB);
- 
- digitalWrite(in1, HIGH);
- digitalWrite(in2, LOW);
- analogWrite(enA, speedA);
+     speedA = 110;
+     speedB = 110;
+     
+     digitalWrite(in3, HIGH);
+     digitalWrite(in4, LOW);
+     analogWrite(enB, speedB);
+     
+     digitalWrite(in1, HIGH);
+     digitalWrite(in2, LOW);
+     analogWrite(enA, speedA);
  }
  
  if (results.value == back){
- speedA = 110;
- speedB = 110;
- directionMovement = 1;
- digitalWrite(in3, LOW);
- digitalWrite(in4, HIGH);
- analogWrite(enB, speedB);
-
- digitalWrite(in2, HIGH);
- digitalWrite(in1, LOW);
- analogWrite(enA, speedA);
+     speedA = 110;
+     speedB = 110;
+     
+     digitalWrite(in3, LOW);
+     digitalWrite(in4, HIGH);
+     analogWrite(enB, speedB);
+    
+     digitalWrite(in2, HIGH);
+     digitalWrite(in1, LOW);
+     analogWrite(enA, speedA);
  }
  
  if (results.value == left){
-   Serial.println("stanga");
- analogWrite(enB, speedB / 2);
- delay(2000);
- analogWrite(enB, speedB);
+     analogWrite(enB, speedB / 2);
+     delay(2000);
+     analogWrite(enB, speedB);
  }
  
  if (results.value == right){
-  Serial.println("dreapta");
- analogWrite(enA, speedA / 2);
- delay(2000);
- analogWrite(enA, speedA);
+     analogWrite(enA, speedA / 2);
+     delay(2000);
+     analogWrite(enA, speedA);
  }
 
  if (results.value == plus){
- // Serial.println("plus");
- if (isStateChange){
- if (speedA != 0)
-    speedA += speedA > 160 ? 0 :30;
- if (speedB != 0)
- speedB += speedB > 160 ? 0 :30;
- }
- analogWrite(enA, speedA);
- analogWrite(enB, speedB);
- Serial.println(speedA);
- Serial.println(speedB);
+     if (isStateChange){
+         if (speedA != 0)
+            speedA += speedA > 160 ? 0 :30;
+         if (speedB != 0)
+         speedB += speedB > 160 ? 0 :30;
+     }
+     analogWrite(enA, speedA);
+     analogWrite(enB, speedB);
  }
 
  if (results.value == minus){
-// Serial.println("plus");
- if (isStateChange){
- speedA -= speedA < 120 ? 0 :30;
- speedB -= speedB < 120 ? 0 :30;
- }
- analogWrite(enA, speedA);
- analogWrite(enB, speedB);
- Serial.println(speedA);
- Serial.println(speedB);
+     if (isStateChange){
+         speedA -= speedA < 120 ? 0 :30;
+         speedB -= speedB < 120 ? 0 :30;
+     }
+     analogWrite(enA, speedA);
+     analogWrite(enB, speedB);
  }
 
  if (results.value == rotateRight){
- if (directionMovement == 0){
- digitalWrite(in2, HIGH);
- digitalWrite(in1, LOW);
- digitalWrite(in3, HIGH);
- digitalWrite(in4, LOW);
- }
- else{
- digitalWrite(in3, HIGH);
- digitalWrite(in4, LOW);
- digitalWrite(in1, LOW);
- digitalWrite(in2, HIGH);
- }
- analogWrite(enA, speedA);
- analogWrite(enB, speedB);
+     digitalWrite(in2, HIGH);
+     digitalWrite(in1, LOW);
+     digitalWrite(in3, HIGH);
+     digitalWrite(in4, LOW);
+     analogWrite(enA, speedA);
+     analogWrite(enB, speedB);
  }
 
  if (results.value == rotateLeft){
- if (directionMovement == 0){
- digitalWrite(in3, LOW);
- digitalWrite(in4, HIGH);
- digitalWrite(in1, HIGH);
- digitalWrite(in2, LOW);
- }
- else{
- digitalWrite(in3, LOW);
- digitalWrite(in4, HIGH);
- digitalWrite(in1, HIGH);
- digitalWrite(in2, LOW);
- }
- analogWrite(enA, speedA);
- analogWrite(enB, speedB);
+     digitalWrite(in2, LOW);
+     digitalWrite(in1, HIGH);
+     digitalWrite(in3, LOW);
+     digitalWrite(in4, HIGH);
+     analogWrite(enA, speedA);
+     analogWrite(enB, speedB);
  }
  
  if (results.value == powerOffOn){
- digitalWrite(in3, LOW);
- digitalWrite(in4, LOW); 
- digitalWrite(in2, LOW);
- digitalWrite(in1, LOW);
- digitalWrite(enA,LOW);
- digitalWrite(enB,LOW);
- speedA = 0;
- speedB = 0;
+     digitalWrite(in3, LOW);
+     digitalWrite(in4, LOW); 
+     digitalWrite(in2, LOW);
+     digitalWrite(in1, LOW);
+     digitalWrite(enA,LOW);
+     digitalWrite(enB,LOW);
+     speedA = 0;
+     speedB = 0;
  }
 }
